a3381126e2bc4a2e743dcfe04b54599f27689491
varsha <varsha@kth.se>
2015-05-02 19:41:53 +0200

    func URL to geoTAG JSON Object

--- src/ReadMetaData.java
+++ src/ReadMetaData.java
@@ -5,25 +5,31 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 
+@SuppressWarnings("deprecation")
 public class ReadMetaData {
 
-	public static void main(String[] args) {
-
-		URL u = getURL("https://www.drupal.org/files/IMG_4692.jpg");
-		System.out.println(u.toString());
+	public static JSONObject getGeoTag(URL u){
 		InputStream in = getInputStream(u);
-
 		Metadata metadata;
+		System.out.println("inside geotag func :");
 		try {
 			metadata = ImageMetadataReader.readMetadata(in);
 			print(metadata);
-			
 			/*
 			 conversion of coordinates into decimal 
 			 */
@@ -33,9 +39,7 @@ public class ReadMetaData {
 				for (Tag tag : directory.getTags()) {
 
 					if (tag.getTagName().contains("GPS")) {
-//						System.out.println(tag);
 					}
-					
 					if(tag.getTagName().equals("GPS Latitude")) {
 						System.out.println(tag.getDescription());
 						lat = tag.getDescription();
@@ -56,11 +60,83 @@ public class ReadMetaData {
 			double decLon = 0, decLat = 0;
 			decLon = cordinatesToDecimal(lon);
 			decLat = cordinatesToDecimal(lat);
-			
+
 			corMap.put("Longitude", decLon);
 			corMap.put("Latitude", decLat);
 			System.out.println(corMap);
-			
+
+			JSONObject ret = getLocationInfo(decLat,decLon);
+			return ret;
+		
+		} catch (ImageProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static void main(String[] args) {
+
+		URL u = getURL("https://www.drupal.org/files/IMG_4692.jpg");
+		InputStream in = getInputStream(u);
+		Metadata metadata;
+		try {
+			metadata = ImageMetadataReader.readMetadata(in);
+			print(metadata);
+
+			/*
+			 conversion of coordinates into decimal 
+			 */
+			String lon = null, lat = null;
+			for (Directory directory : metadata.getDirectories()) {
+
+				for (Tag tag : directory.getTags()) {
+
+					if (tag.getTagName().contains("GPS")) {
+						//						System.out.println(tag);
+					}
+
+					if(tag.getTagName().equals("GPS Latitude")) {
+//						System.out.println(tag.getDescription());
+						lat = tag.getDescription();
+					}
+					else if(tag.getTagName().equals("GPS Longitude")) {
+//						System.out.println(tag.getDescription());
+						lon = tag.getDescription();
+					}
+				}
+
+				if (directory.hasErrors()) {
+					for (String error : directory.getErrors()) {
+						System.err.println("ERROR: " + error);
+					}
+				}
+			}
+			Map<String,Double> corMap = new HashMap<String,Double>();
+			double decLon = 0, decLat = 0;
+			decLon = cordinatesToDecimal(lon);
+			decLat = cordinatesToDecimal(lat);
+
+			corMap.put("Longitude", decLon);
+			corMap.put("Latitude", decLat);
+//			System.out.println(corMap);
+
+			JSONObject ret = getLocationInfo(decLat,decLon);
+//			JSONObject location;
+//			String location_string;
+//			System.out.println(ret);
+			try {
+				//Get JSON Array called "results" and then get the 0th complete object as JSON        
+//				location = ret.getJSONArray("results").getJSONObject(0); 
+				// Get the value of the attribute whose name is "formatted_string"
+				//			    location_string = location.getString("formatted_address");
+				//			    System.out.println("test formattted address:" + location_string);
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+
+			}
 		} catch (ImageProcessingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -78,7 +154,7 @@ public class ReadMetaData {
 		double m = Double.parseDouble(tokens[1].substring(0,tokens[1].length()-1));
 		double s = Double.parseDouble(tokens[2].substring(0,tokens[2].length()-1));
 		boolean isNeg = false;
-		
+
 		if(d<0){
 			isNeg = true;
 			d = -d;
@@ -87,7 +163,7 @@ public class ReadMetaData {
 		if(isNeg){
 			decCord = -decCord;
 		}
-//		System.out.println(decCord);
+		//		System.out.println(decCord);
 		return decCord;
 	}
 	private static URL getURL(String url) {
@@ -101,6 +177,33 @@ public class ReadMetaData {
 		}
 		return null;
 
+	}
+	public static JSONObject getLocationInfo( double lat, double lng) {
+
+		HttpGet httpGet = new HttpGet("http://maps.google.com/maps/api/geocode/json?latlng="+lat+","+lng+"&sensor=false");
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response;
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
+			response = client.execute(httpGet);
+			HttpEntity entity = response.getEntity();
+			InputStream stream = entity.getContent();
+			int b;
+			while ((b = stream.read()) != -1) {
+				stringBuilder.append((char) b);
+			}
+		} catch (ClientProtocolException e) {
+		} catch (IOException e) {
+		}
+
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject = new JSONObject(stringBuilder.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		client.getConnectionManager().shutdown();
+		return jsonObject;
 	}
 
 	private static InputStream getInputStream(URL url) {
